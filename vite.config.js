b42a75fca81d6d147129f77ac45c437055807d28
EncryptedMe <prashantmalviya323@gmail.com>
2026-09-11 10:09:13 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-export default defineConfig({build:{rollupOptions:{input:{auth:resolve(__dirname,"auth.html"),dashboard:resolve(__dirname,"dashboard.html"),reset:resolve(__dirname,"reset.html")}}}});
